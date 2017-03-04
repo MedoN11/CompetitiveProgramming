7f18d7197b375a9b7d9bc284a2414cc0b50fa698
MedoN11 <medonasser951@yahoo.com>
2017-03-04 05:01:06 +0200

    more

--- SPOJ/BHAT007.cpp
+++ SPOJ/BHAT007.cpp
@@ -36,47 +36,55 @@ int len1,len2;
 char num1[MAXN];
 char num2[MAXN];
 
-/// just do regular base addition and check
+// regular base addition
+// test cases for this problem are weak
 int res1[MAXN],res2[MAXN];
 int add(int base,int *arr)
 {
-	int n = 0;
-	int carry = 0;
-	for(int i = len1 - 1, j = len2 - 2 ; i >= 0 || j >= 0 ; --j,--i,++n)
-	{
-		int a = i >= 0 ? num1[i] - '0' : 0;
-		int b = j >= 0 ? num2[j] - '0' : 0;
-		arr[n] = (a + b + carry) % base;
-		carry = (a + b + carry) / base;
-	}
-	while(!arr[n])--n;
-
-	return n;
+    int n = 0;
+    int carry = 0;
+    for(int i = len1 - 1, j = len2 - 1 ; i >= 0 || j >= 0 ; --j,--i,++n)
+    {
+        int a = i >= 0 ? num1[i] - '0' : 0;
+        int b = j >= 0 ? num2[j] - '0' : 0;
+        arr[n] = (a + b + carry) % base;
+        carry = (a + b + carry) / base;
+    }
+    while(carry > 0)
+    {
+        arr[n++] = carry % base;
+        carry /= base;
+    }
+    --n;
+    
+    while(!arr[n])--n;
+    
+    return n;
 }
 
 bool same(int n)
 {
-	for(int i = 0 ; i <= n ; ++i)
-	{
-		if(res1[i] != res2[i])
-			return 0;
-	}
-	return 1;
+    for(int i = 0 ; i <= n ; ++i)
+    {
+        if(res1[i] != res2[i])
+        return 0;
+    }
+    return 1;
 }
 int main()
 {
-	int t;
-	int b1,b2;
-	scanf("%d",&t);
-	while(t--)
-	{
-		scanf("%s %s %d %d",num1,num2,&b1,&b2);
-		len1 = strlen(num1);
-		len2 = strlen(num2);
-		int n = add(b1,res1);
-		int m = add(b2,res2);
-		if(n == m && same(n))
-			puts("yes");
-		else puts("no");
-	}
+    int t;
+    int b1,b2;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%s %s %d %d",num1,num2,&b1,&b2);
+        len1 = strlen(num1);
+        len2 = strlen(num2);
+        int n = add(b1,res1);
+        int m = add(b2,res2);
+        if(n == m && same(n))
+        puts("yes");
+        else puts("no");
+    }
 }
