b21e83bd1905ac3c30e085c37740489549901edf
MedoN11 <medonasser951@yahoo.com>
2017-03-04 04:39:47 +0200

    more

---
+++ SPOJ/BHAT007.cpp
@@ -0,0 +1,82 @@
+#include <cmath>
+#include <map>
+#include<fstream>
+#include<algorithm>
+#include <iostream>
+#include<numeric>
+#include<utility>
+#include<functional>
+#include<stdio.h>
+#include <iomanip>
+#include<assert.h>
+#include<memory.h>
+#include<bitset>
+#include<algorithm>
+#include <cassert>
+#include <stack>
+#include <ctime>
+#include <queue>
+#include <vector>
+#include <iterator>
+#include <math.h>
+#include<cstring>
+#include <cmath>
+#include <complex>
+#include <sstream>
+#include<cstring>
+#include <complex>
+#include<cstdlib>
+#include <set>
+#include <cstdio>
+#include<map>
+using namespace std;
+#define ll long long
+#define MAXN 1000001
+int len1,len2;
+char num1[MAXN];
+char num2[MAXN];
+
+/// just do regular base addition and check
+int res1[MAXN],res2[MAXN];
+int add(int base,int *arr)
+{
+	int n = 0;
+	int carry = 0;
+	for(int i = len1 - 1, j = len2 - 2 ; i >= 0 || j >= 0 ; --j,--i,++n)
+	{
+		int a = i >= 0 ? num1[i] - '0' : 0;
+		int b = j >= 0 ? num2[j] - '0' : 0;
+		arr[n] = (a + b + carry) % base;
+		carry = (a + b + carry) / base;
+	}
+	while(!arr[n])--n;
+
+	return n;
+}
+
+bool same(int n)
+{
+	for(int i = 0 ; i <= n ; ++i)
+	{
+		if(res1[i] != res2[i])
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	int t;
+	int b1,b2;
+	scanf("%d",&t);
+	while(t--)
+	{
+		scanf("%s %s %d %d",num1,num2,&b1,&b2);
+		len1 = strlen(num1);
+		len2 = strlen(num2);
+		int n = add(b1,res1);
+		int m = add(b2,res2);
+		if(n == m && same(n))
+			puts("yes");
+		else puts("no");
+	}
+}
