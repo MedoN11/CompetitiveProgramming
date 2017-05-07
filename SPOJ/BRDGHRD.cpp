b0b6aac8d31c5b30cd0108cb7c91760090a21fa8
Medo Nasser <Medo@Mohameds-MacBook-Air.local>
2017-05-07 00:48:15 -0700

    more

--- SPOJ/BRDGHRD.cpp
+++ SPOJ/BRDGHRD.cpp
@@ -47,20 +47,17 @@ int LongestIncreasingSubsequenceLength(std::vector<int> &v) {
         return 0;
 
     std::vector<int> tail(v.size(), 0);
-    int length = 1; // always points empty slot in tail
+    int length = 1;
 
     tail[0] = v[0];
     for (size_t i = 1; i < v.size(); i++) {
         if (v[i] < tail[0])
-            // new smallest value
             tail[0] = v[i];
         else if (v[i] > tail[length-1])
-            // v[i] extends largest subsequence
+
             tail[length++] = v[i];
         else
-            // v[i] will become end candidate of an existing subsequence or
-            // Throw away larger elements in all LIS, to make room for upcoming grater elements than v[i]
-            // (and also, v[i] would have already appeared in one of LIS, identify the location and replace it)
+
             tail[CeilIndex(tail, -1, length-1, v[i])] = v[i];
     }
 
