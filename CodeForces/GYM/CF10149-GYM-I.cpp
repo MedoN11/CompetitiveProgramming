efd40a99b0d17b59827263294d8de4298c9357b5
Medo Nasser <Medo@Mohameds-MacBook-Air.local>
2017-08-30 23:04:14 -0700

    more

---
+++ CodeForces/GYM/CF10149-GYM-I.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdio>
+#include<list>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include <functional>
+#include<stdio.h>
+#include<assert.h>
+#include<stack>
+#include<sstream>
+#include <cstdlib>
+#include<map>
+#include<algorithm>
+#include<iostream>
+#include<set>
+#include<utility>
+#include<memory.h>
+#include<string>
+#include<vector>
+#include<numeric>
+using namespace std;
+#define ll  long long
+#define pb push_back
+#define sz(v)               ((int)((v).size()))
+#define all(x)          (x).begin(),(x).end()
+#define REP(i, v)       for(int i=0;i<sz(v);i++)
+typedef pair<int,int> pii;
+#define ios std::ios_base::sync_with_stdio(false);
+
+int main()
+{
+
+
+
+	int tc;
+	scanf("%d",&tc);
+	int a,b;
+	while(tc--)
+	{
+		scanf("%d %d",&a,&b);
+		int grund = a % 2 + b % 2;
+		if(grund)
+			puts("hasan");
+		else puts("abdullah");
+	}
+}
