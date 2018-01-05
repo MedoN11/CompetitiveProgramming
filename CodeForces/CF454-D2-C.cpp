09a7b34f8cf93d6fcc0838ddf6fe852379796a3f
Medo Nasser <medonasser@Medos-MacBook-Pro.local>
2018-01-05 02:18:18 +0200

    more

---
+++ CodeForces/CF454-D2-C.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include<list>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include <functional>
+#include<stdio.h>
+#include<assert.h>
+#include<stack>
+#include<sstream>
+#include <cstdlib>
+#include<map>
+#include<algorithm>
+#include<iostream>
+#include<set>
+#include<utility>
+#include<memory.h>
+#include<string>
+#include<vector>
+#include<numeric>
+#include <unordered_map>
+using namespace std;
+#define ll  long long
+#define pb push_back
+#define fi(ss) freopen (ss,"r",stdin)
+#define fo(ss) freopen (ss,"w",stdout)
+#define sz(v)               ((int)((v).size()))
+#define all(x)          (x).begin(),(x).end()
+#define REP(i, v)       for(int i=0;i<sz(v);i++)
+#define lp(i,n) for(int i = 0 ; i < n ; ++i)
+typedef pair<int,int> pii;
+#define ios std::ios_base::sync_with_stdio(false);
+using namespace std;
+int n,m;
+int main() {
+	scanf("%d %d",&m,&n);
+	double ans = 0;
+	double den = pow(1.0/m,n);
+	for (int i = 1 ; i <= m ; ++i) {
+		ans += i *( pow((1.0*i) / m,n) - pow((1.0*i - 1.0)/m,n));
+	}
+	printf("%.6f",ans);
+
+}
