65617dfa1389e9aa748bec89b27657908ea75215
Medo <medonasser@Medos-MacBook-Pro.local>
2018-01-30 20:33:37 +0200

    more

--- TopCoder/SRM331-D1-500.cpp
+++ TopCoder/SRM331-D1-500.cpp
@@ -38,6 +38,49 @@ typedef pair<int,int> pii;
 #define F first
 #define S second
 #define MP make_pair
+struct Shopping {
+	int minNumber(int x, vector<int> q);
+};
+#include <iostream>
+#include <cstdio>
+#include<list>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include <functional>
+#include<stdio.h>
+#include<assert.h>
+#include<stack>
+#include<sstream>
+#include <cstdlib>
+#include<map>
+#include<algorithm>
+#include<iostream>
+#include<set>
+#include<utility>
+#include<memory.h>
+#include<string>
+#include<vector>
+#include<numeric>
+using namespace std;
+#define ios std::ios_base::sync_with_stdio(false);
+#define ll  long long
+#define pb push_back
+#define fi(ss) freopen (ss,"r",stdin)
+#define fo(ss) freopen (ss,"w",stdout)
+#define sz(v)               ((int)((v).size()))
+#define all(x)          (x).begin(),(x).end()
+#define REP(i, v)       for(int i=0;i<sz(v);i++)
+#define lp(i,n) for(int i = 0 ; i < n ; ++i)
+#define hash ___hash
+#define next ___next
+#define prev ___prev
+#define left ___left
+#define time __time
+typedef pair<int,int> pii;
+#define F first
+#define S second
+#define MP make_pair
 int n,s;
 vector<int> v;
 /*
@@ -45,7 +88,10 @@ vector<int> v;
  *  https://github.com/MedoN11/CompetitiveProgramming/blob/master/Timus/1515.cpp
  *  This DP uses the above idea, and bruteforces all combinations that can represent anything from 1 to N
  *  along those DP minimizes the number of coins used.
- *
+ *  If you look closely, DP is not needed
+ *  We can transform the problem into
+ *  Given S and n numbers, represent any number >= S using minimum numbers
+ *  solution is obviously greedy
  */
 int dp[1001];
 struct Shopping {
@@ -68,4 +114,16 @@ int Shopping::minNumber(int x,vector<int> q) {
 	memset(dp,-1,sizeof(dp));
 	if(solve(0) == 1 << 25) return -1;
 	return dp[0];
+}
+int Shopping::minNumberGreedy(int x,vector<int> v) {
+	sort(all(v));
+	if(v[0] != 1) return -1;
+	int ind = 0, curr = 0;
+	int iter = 0;
+	while(curr < x) {
+		while(ind + 1 < sz(v) && ( (v[ind + 1] - curr == 1) || (curr >= v[ind + 1]))) ++ind;
+		curr += v[ind];
+		++iter;
+	}
+	return iter;
 }
