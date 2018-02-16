8a7d12f97e1bbf615ef527f9dc222a062240df9c
Medo <medonasser@Medos-MacBook-Pro.local>
2018-02-17 00:45:14 +0200

    more

--- CodeForces/CF101490-GYM-E.cpp
+++ CodeForces/CF101490-GYM-E.cpp
@@ -35,31 +35,78 @@ using namespace std;
 #define prev ___prev
 #define left ___left
 #define time __time
-typedef pair<int,int> pii;
+typedef pair<ll,ll> pii;
 #define F first
 #define S second
 #define MP make_pair
-ll n,b;
-const int maxn = 4005;
-int dp2[maxn][maxn];
-int dp1[maxn];
-int arrive[maxn];
-int main() {
-	cin >> n;
-	for (int i = 0 ; i < n ; ++i) {
-		scanf("%d",&arrive[i]);
+const int maxn = 1000*1000 + 5;
+string ans = "";
+int n,m,p;
+vector<pii> g[maxn];
+ll block = 0;
+ll best = 0;
+ll DIS[maxn];
+ll oo = 1LL << 55;
+ll diji(int idx) {
+	for(int i = 0 ; i < n; i++)
+	{
+		DIS[i] = oo;
 	}
-	for (int i = 0 ; i < n ; ++i) {
-		for (int j = i ; j < n ; ++j) {
-			dp2[i][j] = max(120 + (j - i + 1) * 20,140 + arrive[j] - arrive[i] - 1800);
-		}
-	}
-	for (int i = 0 ; i < n ; ++i) {
-		dp1[i] = dp2[0][i];
-		for (int j = 1 ; j <= i ; ++j) {
-			dp1[i] = min(dp1[i],dp2[j][i] + dp1[j - 1]);
-		}
-	}
-	cout << dp1[n - 1] << "\n";
+	priority_queue<pair<ll,ll>,vector<pair<ll,ll> >,greater<pair<ll,ll> > > PQ;
+	DIS[idx]= 0;
+	PQ.push(make_pair(0,idx));
+	while(!PQ.empty())
+	{
+		pair<ll,ll> out = PQ.top(); PQ.pop();
+		int index = out.second;
+		ll dis = out.first;
+		if(DIS[index] < dis) continue;
+		int sz = (int)(g[index].size());
+		for(int i = 0 ; i < sz ; i++)
+		{
+			pair<ll,ll> next = g[index][i];
+			if(next.second > block) continue;
+			ll cost = (ll)(1LL*next.second + DIS[index]);
+			if(cost >= DIS[next.first]) continue;
+			DIS[next.first] = cost;
+			PQ.push(make_pair(cost,next.first));
 
+		}
+	}
+	return DIS[n - 1];
+}
+ll ok(ll x) {
+	return 100*x <= 100*best + best * p;
+}
+bool can(ll x) {
+	block = x;
+	ll res = diji(0);
+	return ok(res);
+
+}
+int main() {
+	ios
+	scanf("%d %d %d",&n,&m,&p);
+	for (int i = 0 ; i < m ; ++i) {
+		int u,v,c;
+		scanf("%d %d %d",&u,&v,&c);
+		--u,--v;
+		g[u].pb(pii(v,c));
+		g[v].pb(pii(u,c));
+	}
+	block = 1LL << 55;
+	best = diji(0);
+	cerr << best << "\n";
+	ll lo = 1 , hi = 1LL << 32;
+	ll ans = -1;
+	while (lo <= hi) {
+		ll mid = lo + (hi - lo) / 2;
+		if(can(mid)) {
+			ans = mid;
+			hi = mid - 1;
+		}
+		else lo = mid + 1;
+	}
+
+	cout << ans << "\n";
 }
